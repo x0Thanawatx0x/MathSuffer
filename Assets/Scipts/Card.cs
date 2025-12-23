3e1731a77d6cea5bd777b2ca62a028c2c5d56938
x0Thanawatx0x <tanami2077@gmail.com>
2025-12-24 02:17:30 +0700

    Finish

---
+++ Assets/Scipts/Card.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [HideInInspector] public int cardID;
+    [HideInInspector] public bool isFlipped = false;
+
+    public Image cardImage; // ต้องลากใน Inspector
+
+    private Sprite frontSprite;
+    private Sprite backSprite;
+
+    private Button button;
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        // ✅ ใช้ Awake แทน Start (สำคัญมาก)
+        button = GetComponent<Button>();
+        gameManager = FindObjectOfType<GameManager>();
+
+        if (cardImage == null)
+            cardImage = GetComponent<Image>();
+    }
+
+    public void Setup(Sprite front, Sprite back)
+    {
+        frontSprite = front;
+        backSprite = back;
+
+        FlipDown();
+        gameObject.SetActive(true);
+    }
+
+    void OnEnable()
+    {
+        if (button != null)
+            button.onClick.AddListener(OnCardClicked);
+    }
+
+    void OnDisable()
+    {
+        if (button != null)
+            button.onClick.RemoveListener(OnCardClicked);
+    }
+
+    void OnCardClicked()
+    {
+        if (isFlipped || gameManager.isChecking) return;
+
+        FlipUp();
+        gameManager.CardSelected(this);
+    }
+
+    public void FlipUp()
+    {
+        isFlipped = true;
+        cardImage.sprite = frontSprite;
+        button.interactable = false;
+    }
+
+    public void FlipDown()
+    {
+        isFlipped = false;
+
+        if (cardImage != null)
+            cardImage.sprite = backSprite;
+
+        if (button != null)
+            button.interactable = true;
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scipts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Cards")]
+    public List<Card> cards;
+
+    [Header("Sprites Reference")]
+    public List<Sprite> frontSprites;   // รูปหน้า (ไม่ซ้ำ)
+    public Sprite backSprite;            // รูปหลัง (รูปเดียว)
+
+    public float flipBackDelay = 1f;
+    [HideInInspector] public bool isChecking;
+
+    private Card firstCard;
+    private Card secondCard;
+
+    void Start()
+    {
+        SetupCards();
+        ShufflePositions();
+    }
+
+    void SetupCards()
+    {
+        if (cards.Count % 2 != 0 || frontSprites.Count < cards.Count / 2)
+        {
+            Debug.LogError("จำนวนการ์ดหรือรูปไม่ถูกต้อง");
+            return;
+        }
+
+        List<int> ids = new List<int>();
+
+        for (int i = 0; i < cards.Count / 2; i++)
+        {
+            ids.Add(i);
+            ids.Add(i);
+        }
+
+        for (int i = 0; i < ids.Count; i++)
+        {
+            int rand = Random.Range(i, ids.Count);
+            (ids[i], ids[rand]) = (ids[rand], ids[i]);
+        }
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            cards[i].cardID = ids[i];
+            cards[i].Setup(frontSprites[ids[i]], backSprite);
+        }
+    }
+
+    public void CardSelected(Card card)
+    {
+        if (isChecking) return;
+
+        if (firstCard == null)
+        {
+            firstCard = card;
+        }
+        else
+        {
+            secondCard = card;
+            StartCoroutine(CheckMatch());
+        }
+    }
+
+    IEnumerator CheckMatch()
+    {
+        isChecking = true;
+        yield return new WaitForSeconds(0.3f);
+
+        if (firstCard.cardID == secondCard.cardID)
+        {
+            firstCard.Hide();
+            secondCard.Hide();
+        }
+        else
+        {
+            yield return new WaitForSeconds(flipBackDelay);
+            firstCard.FlipDown();
+            secondCard.FlipDown();
+        }
+
+        firstCard = null;
+        secondCard = null;
+        isChecking = false;
+    }
+
+    void ShufflePositions()
+    {
+        foreach (Card card in cards)
+        {
+            card.transform.SetSiblingIndex(Random.Range(0, cards.Count));
+        }
+    }
+}
